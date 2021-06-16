0a8c692ece1e76337e8071c5abc51ad521c09387
PRUDHVI RAJ ANNA <PRUDHVI RAJ ANNA@DESKTOP-TSK37KQ>
2021-06-16 09:05:21 +0530

    addMetod

---
+++ OCSM11AutomationMaven/src/test/java/com/vtiger/testscripts/DemoTest.java
@@ -0,0 +1,12 @@
+package com.vtiger.testscripts;
+
+import org.testng.annotations.Test;
+
+public class DemoTest 
+{
+  @Test
+  public void addMethod()
+  {
+	  System.out.println("Welcome To TestNG");
+  }
+}
